b41d7e032686dc09e8d839ec47673eeed3445ba1
dongchuanliang <244744021@qq.com>
2018-05-11 09:09:27 +0800

    test

---
+++ mylibrary/src/main/java/com/ddsfec/mylibrary/Xstring.java
@@ -0,0 +1,14 @@
+package com.ddsfec.mylibrary;
+
+import android.util.Log;
+
+/**
+ * Created by dongchuanliang on 2018/5/11.
+ */
+
+public class Xstring {
+
+    public void  toLog(){
+        Log.e("ddsfec", "toLog: ");
+    }
+}
